dbeb1f09f7abea9578d9f7b1db7c0bfa9c434cc3
Mojdeh Pajang <mojde.pajang@gmail.com>
2023-11-23 14:07:53 +0100

    add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 // Import the framework and instantiate it
-import Fastify from 'fastify'
+import Fastify from 'fastify';
 const fastify = Fastify({
-  logger: true
-})
+	logger: true,
+});
 
 // Declare a route
-fastify.get('/', async function handler (request, reply) {
-  return { hello: 'world' }
-})
+fastify.get('/', async function handler(request, reply) {
+	return { hello: 'world' };
+});
 
 // Run the server!
-fastify.listen({ port: 3000 }).then(() => {
-  console.log("server runs in http://localhost:3000")
-}).catch((err)=>{fastify.log.error(err)})
+fastify
+	.listen({ port: 3000 })
+	.then(() => {
+		console.log('server runs in http://localhost:3000');
+	})
+	.catch((err) => {
+		fastify.log.error(err);
+	});
