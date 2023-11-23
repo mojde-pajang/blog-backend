03a86c5e3c829bb1f3a6c61e7baa3c0c7d2ecb6d
Mojdeh Pajang <mojde.pajang@gmail.com>
2023-11-23 14:06:13 +0100

    create fastify server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
+// Import the framework and instantiate it
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
 
-console.log("blog")
+// Declare a route
+fastify.get('/', async function handler (request, reply) {
+  return { hello: 'world' }
+})
+
+// Run the server!
+fastify.listen({ port: 3000 }).then(() => {
+  console.log("server runs in http://localhost:3000")
+}).catch((err)=>{fastify.log.error(err)})
