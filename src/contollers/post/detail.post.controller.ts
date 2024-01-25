2dcd348eab802b5c33aa94068f8720753291dcf9
Mojdeh Pajang <mojde.pajang@gmail.com>
2024-01-25 14:01:11 +0100

    retrive image in blog detail

--- src/contollers/post/detail.post.controller.ts
+++ src/contollers/post/detail.post.controller.ts
@@ -1,10 +1,11 @@
 import { StatusCodes } from 'http-status-codes';
 import { Post } from '../../models/post.model';
+import { Gallery } from '../../models/gallery.model';
 
 export const detailPost = async (request: any, reply: any) => {
 	const { id } = request.params;
 	try {
-		const post = await Post.findOne({ where: { id: id } });
+		const post = await Post.findOne({ where: { id: id }, include: Gallery });
 		return reply.status(StatusCodes.OK).send({ post });
 	} catch (error) {
 		return reply.status(StatusCodes.NOT_FOUND).send({ error });
