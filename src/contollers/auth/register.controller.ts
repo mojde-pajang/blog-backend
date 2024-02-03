ee7f066701d465ae1ea6b7b5877fd79daf567c87
Mojdeh Pajang <mojde.pajang@gmail.com>
2024-02-03 15:33:31 +0100

    update register controller

--- src/contollers/auth/register.controller.ts
+++ src/contollers/auth/register.controller.ts
@@ -1,35 +1,42 @@
 import { FastifyInstance } from 'fastify/types/instance';
 import { fastify } from '../../index';
+
 export const registerController = async (request: any, reply: any) => {
 	const { firstName, lastName, age, email, password, roleName } = request.body;
 	console.log(123, request.body);
-	const models = request.server.sequelize.models;
-	const userRole = roleName ? roleName : 'Visitor';
 
 	try {
-		const userExists = await models.User.findOne({ where: { email: email } });
+		const { User, Role } = request.server.sequelize.models;
+
+		const userExists = await User.findOne({ where: { email: email } });
 
 		if (userExists) {
 			fastify.log.error('Duplicate user');
-			return reply.status(400).send({ message: 'user exists' });
+			return reply.status(400).send({ message: 'User exists' });
 		}
-		let role = await models.Role.findOne({ where: { roleName: userRole } });
+
+		let role = await Role.findOne({ where: { roleName: roleName || 'Visitor' } });
 
 		if (!role) {
-			const [newRole, created] = await models.Role.findOrCreate({ where: { roleName: 'Visitor' } });
+			const [newRole, created] = await Role.findOrCreate({ where: { roleName: 'Visitor' } });
 			role = newRole;
 		}
+
 		const hashedPassword = await fastify.bcrypt.hash(password);
-		const newUser = await models.User.create({
+
+		const newUser = await User.create({
 			firstName,
 			lastName,
 			age,
 			email,
 			password: hashedPassword,
 		});
-		newUser.setRole(role);
+
+		await newUser.setRole(role);
+
 		return reply.status(200).send({ newUser });
 	} catch (error) {
-		console.log(error);
+		console.error(error);
+		return reply.status(500).send({ message: 'Internal Server Error' });
 	}
 };
