12ce20f7b946bef4fc41327791e4f0abef3043a2
Mojdeh Pajang <mojde.pajang@gmail.com>
2024-02-03 16:20:51 +0100

    update database connection

--- src/plugins/db.ts
+++ src/plugins/db.ts
@@ -6,14 +6,18 @@ const fp = require('fastify-plugin');
 
 module.exports = fp(async function (fastify: any, opts: any, done: any) {
 	console.log(111, process.env.DATABASE_NAME, process.env.DATABASE_URL);
-	const sequelize = process.env.DATABASE_URL
-		? new Sequelize(process.env.DATABASE_URL)
-		: new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
-				dialect: 'postgres',
-				host: process.env.DATABASE_HOST,
-				port: process.env.DATABASE_PORT,
-				database: process.env.DATABASE_NAME,
-		  });
+	const sequelize = new Sequelize(
+		process.env.DATABASE_NAME,
+		process.env.DATABASE_USERNAME,
+		process.env.DATABASE_PASSWORD,
+		{
+			dialect: 'postgres',
+			host: process.env.DATABASE_HOST,
+			port: process.env.DATABASE_PORT,
+			database: process.env.DATABASE_NAME,
+			ssl: true,
+		},
+	);
 
 	try {
 		await sequelize.authenticate();
